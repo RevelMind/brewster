910be8292639a9efbff7823e2e89e3a2fcd37244
Zack <mcodikid@gmail.com>
2020-03-10 19:19:36 -0600

    Move things around.

--- index.js
+++
@@ -1,7 +0,0 @@
-var colors = require("colors");
-var readline = require("readline-sync");
-var release = false;
-
-console.log(("[" + (release ? "RELEASE".green : "DEV".red) + "]") + " Luna " + require("./package.json").version);
-console.log("Copyright (c) RevelMind 2020");
-
