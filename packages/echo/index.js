cae305f6affdfbbbb01efbb2d3dee4b0f2525bbf
Zack <mcodikid@gmail.com>
2020-03-11 14:26:08 -0600

    Undo some changes.

--- packages/echo/index.js
+++ packages/echo/index.js
@@ -1,7 +1,11 @@
+let program = require('commander');
+let path = require('path')
+let config = require(path.join(__dirname, 'package.json'));
+
 module.exports = {
     onCommand: function(cmd, args) {
         if(cmd.toLowerCase() == 'echo') {
-            console.log(args.join(" "));
+            console.log(args.join(' '));
             return true;
         }
         return false;
